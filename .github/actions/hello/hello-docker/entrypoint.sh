2ef02036d61e9d9f14f606f44b4184344f7ae8e9
GaspardPierre <pierredillard@gmail.com>
2024-04-25 18:46:43 +0200

    hello docker

---
+++ .github/actions/hello/hello-docker/entrypoint.sh
@@ -0,0 +1,7 @@
+#!bin/sh
+echo"::debug::Running entrypoint.sh"
+echo "Hello $1"
+echo "INPUT_WHO_TO_GREET: $NPUT_WHO_TO_GREET "
+time=$(date)
+echo "time=$time" >> $GITHUB_OUTPUT
+echo "HELLO_TIME=$time " >> $GITHUB_ENV
